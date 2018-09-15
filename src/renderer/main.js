25765f7f0c3467a17b4bc8872dd606656eb59fe4
bazhijing <ljm@whu.edu.cn>
2018-09-15 21:00:04 +0800

    init

---
+++ src/renderer/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+import 'font-awesome/css/font-awesome.min.css'
+import ElementUI from 'element-ui'
+import './assets/eletheme/index.css'
+Vue.use(ElementUI)
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
